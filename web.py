cc7fed5af484600967678aed9fa658fa486df7a3
Ferret0310 <plus556612@gmail.com>
2026-05-06 08:51:40 +0800

    Update web.py

--- web.py
+++ web.py
@@ -8,11 +8,12 @@ df = pd.read_excel(file_path)
 st.title("蚊蟲實驗自動化查詢系統")
 
 # 2. 建立側邊欄篩選器 (自動生成下拉選單)
-year = st.sidebar.selectbox("請選擇年度", df["年度"].unique())
+year = st.sidebar.selectbox("請選擇年度", df["年"].unique())
 city = st.sidebar.selectbox("請選擇縣市", df["縣市"].unique())
+method = st.sidebar.selectbox("請選實驗方法",df["實驗方法"].unique())
 
 # 3. 套用你原本寫的篩選邏輯
-result = df[(df["年"] == year) & (df["縣市"] == city)]
+result = df[(df["年"] == year) & (df["縣市"] == city) & (df["實驗方法"] == method)]
 
 # 4. 顯示結果
 st.write(f"篩選出 {year} 年，{city} 的實驗紀錄：")
