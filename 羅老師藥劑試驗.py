54579db314238edb94eafd0c80e523092ec54ca8
Ferret0310 <plus556612@gmail.com>
2026-05-06 16:26:25 +0800

    Update 羅老師藥劑試驗.py

--- 羅老師藥劑試驗.py
+++ 羅老師藥劑試驗.py
@@ -6,13 +6,17 @@ file_path = "羅老師藥劑試驗.xlsx"
 df = pd.read_excel(file_path)
 
 st.title("羅老師藥劑實驗自動化查詢系統")
-
+unique_pesticides = df["藥劑"].unique().tolist()
+all_pesticides = ["全部"] + unique_pesticides
 # 2. 建立側邊欄篩選器 (自動生成下拉選單)
 year = st.sidebar.selectbox("請選擇年度", df["年"].unique())
-pesticides = st.sidebar.selectbox("請選擇藥劑", df["藥劑"].unique())
+pesticides = st.sidebar.selectbox("請選擇藥劑",all_pesticides)
 
 # 3. 套用你原本寫的篩選邏輯
-result = df[(df["年"] == year) & (df["藥劑"] == pesticides)]
+result = df[(df["年"] == year)]
+if pesticides != "全部":
+    result = result[result["藥劑"] == pesticides]
+
 count = len(result)
 # 4. 顯示結果
 st.write(f"篩選出 {year} 年，所用藥劑，共{count}筆。")
