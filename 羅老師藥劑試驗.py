b9ca9e55e90643be31e9e9ad60bfd3ac52e909a7
Ferret0310 <plus556612@gmail.com>
2026-05-06 12:01:59 +0800

    Create 羅老師藥劑試驗.py

---
+++ 羅老師藥劑試驗.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import os
+file_path = "羅老師藥劑試驗.xlsx"
+# 1. 讀取資料
+df = pd.read_excel(file_path)
+
+st.title("羅老師藥劑實驗自動化查詢系統")
+
+# 2. 建立側邊欄篩選器 (自動生成下拉選單)
+year = st.sidebar.selectbox("請選擇年度", df["年"].unique())
+pesticides = st.sidebar.selectbox("請選擇藥劑", df["藥劑"].unique())
+
+
+st.dataframe(result.reset_index(drop=True))
+# 3. 套用你原本寫的篩選邏輯
+result = df[(df["年"] == year) & (df["藥劑"] == pesticides)]
+count = len(result)
+# 4. 顯示結果
+st.write(f"篩選出 {year} 年，所用藥劑，共{count}筆。")
+st.dataframe(result)
